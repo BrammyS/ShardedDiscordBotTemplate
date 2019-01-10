21a099a14521cabe8d9c1212d15495304ee801aa
BramEsendam <bram.esendam@gmail.com>
2019-01-10 21:02:38 +0100

    Added shards & ping command

---
+++ Bot/Discord/Commands/BotInfoCommands/PingCommand.cs
@@ -0,0 +1,54 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using Bot.Discord.Helpers;
+using Bot.Logger.Interfaces;
+using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+
+namespace Bot.Discord.Commands.BotInfoCommands
+{
+    public class PingCommand : ModuleBase<SocketCommandContext>
+    {
+        private readonly ILogger _logger;
+        private readonly DiscordShardedClient _shardedClient;
+        private readonly EmbedBuilder _embed;
+
+        public PingCommand(ILogger logger, DiscordShardedClient shardedClient)
+        {
+            _logger = logger;
+            _shardedClient = shardedClient;
+            _embed = new EmbedBuilder();
+        }
+
+        [Command("ping", RunMode = RunMode.Async)]
+        [RequireBotPermission(GuildPermission.EmbedLinks)]
+        public async Task PingAsync()
+        {
+            _embed.WithTitle("Info for" + Context.User.Username);
+            _embed.WithDescription($"{Context.Client.Latency} ms");
+            _embed.WithColor(new Color(255, 255, 255));
+            await ReplyAsync("", false, _embed.Build()).ConfigureAwait(false);
+            _logger.Log($"Server: {Context.Guild}, Id: {Context.Guild.Id} || ShardId: {Context.Client.ShardId} || Channel: {Context.Channel} || User: {Context.User} || Used: invite");
+        }
+
+        [Command("shards", RunMode = RunMode.Async)]
+        [RequireBotPermission(GuildPermission.EmbedLinks)]
+        [RequireBotPermission(GuildPermission.SendMessages)]
+        public async Task ShardsAsync()
+        {
+            _embed.WithTitle("Shard info for " + Context.User.Username);
+            foreach (var shard in _shardedClient.Shards)
+            {
+                _embed.AddField($"Shard: {shard.ShardId} {ShardStatusTypeEmoji.GetStatusEmoji(shard.Latency)}", $"{shard.Latency} ms\n" +
+                                                                                                                $"{shard.Guilds.Count} Servers\n" +
+                                                                                                                $"{shard.Guilds.Sum(x => x.MemberCount)} Members", true);
+            }
+            _embed.WithDescription($"Average ping: {_shardedClient.Shards.Average(x => x.Latency)} ms");
+            _embed.WithFooter($"You are on shard: {Context.Client.ShardId}");
+            _embed.WithColor(new Color(255, 255, 255));
+            await ReplyAsync("", false, _embed.Build()).ConfigureAwait(false);
+            _logger.Log($"Server: {Context.Guild}, Id: {Context.Guild.Id} || ShardId: {Context.Client.ShardId} || Channel: {Context.Channel} || User: {Context.User} || Used: shards");
+        }
+    }
+}
---
+++ Bot/Discord/Helpers/ShardStatusTypeEmoji.cs
@@ -0,0 +1,12 @@
+﻿namespace Bot.Discord.Helpers
+{
+    public static class ShardStatusTypeEmoji
+    {
+        public static string GetStatusEmoji(int latency)
+        {
+            if (latency < 200) return "<:GreenStatus:533010751229526032>";
+            if (latency > 200 && latency < 500) return "<:OrangeStatus:533010753196654600>";
+            return "<:RedStatus:533010751074467851>";
+        }
+    }
+}
