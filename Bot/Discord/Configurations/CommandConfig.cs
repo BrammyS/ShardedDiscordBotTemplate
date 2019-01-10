b02b5feb28d32419b42f1811321152cc699a72e2
BramEsendam <bram.esendam@gmail.com>
2019-01-10 19:43:38 +0100

    Added CommandConfig

---
+++ Bot/Discord/Configurations/CommandConfig.cs
@@ -0,0 +1,18 @@
+﻿using Discord;
+using Discord.Commands;
+
+namespace Bot.Discord.Configurations
+{
+    public static class CommandConfig
+    {
+        public static CommandService GetDefault()
+        {
+            return new CommandService(new CommandServiceConfig
+            {
+                CaseSensitiveCommands = false,
+                LogLevel = LogSeverity.Info,
+                DefaultRunMode = RunMode.Async
+            });
+        }
+    }
+}
