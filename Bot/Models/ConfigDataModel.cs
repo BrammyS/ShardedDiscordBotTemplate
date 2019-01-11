94251ba97d4b374d2f5c841e66c33febb0a953ed
Bram Esendam <bram.esendam@gmail.com>
2019-01-11 10:34:27 +0100

    Added BotLists

--- Bot/Models/ConfigDataModel.cs
+++ Bot/Models/ConfigDataModel.cs
@@ -5,8 +5,5 @@
         public string Token;
         public ulong DiscordBotId;
         public int RestartTime;
-        public string DiscordBotsOrgApiToken;
-        public string DiscordBotsApiToken;
-        public string DivineDiscordBotsApiToken;
     }
 }
