c6e22154d4da5df818b123c57e2d88fd285598a8
Bram Esendam <bram.esendam@gmail.com>
2019-01-11 11:47:45 +0100

    added returns docs to errorhandler

--- Bot.Interfaces/Discord/EventHandlers/CommandHandlers/IErrorHandler.cs
+++ Bot.Interfaces/Discord/EventHandlers/CommandHandlers/IErrorHandler.cs
@@ -7,19 +7,21 @@ namespace Bot.Interfaces.Discord.EventHandlers.CommandHandlers
 
 
         /// <summary>
-        /// Gets the default embedded error message.
+        /// Embeds the default embedded error message.
         /// </summary>
         /// <param name="commandName">The command the user used.</param>
         /// <param name="message">The error message you want to show.</param>
         /// <param name="exception">The exception that will be logged .</param>
+        /// <returns>The embedded error message.</returns>
         EmbedBuilder GetDefaultError(string commandName, string message, string exception);
 
 
         /// <summary>
-        /// Gets a embedded error message.
+        /// Embeds a embedded error message.
         /// </summary>
         /// <param name="title">The title the message should have.</param>
         /// <param name="description">The description the message will have.</param>
+        /// <returns>The embedded error message.</returns>
         EmbedBuilder EmbedError(string title, string description);
     }
 }
