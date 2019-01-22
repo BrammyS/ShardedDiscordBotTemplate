f0aa656a7af195f55b50d1b7b00fb352660d4ff7
BramEsendam <bram.esendam@gmail.com>
2019-01-22 21:35:26 +0100

    Create UsersConfiguration.cs

---
+++ Bot.Persistence.EntityFrameWork/Configurations/UsersConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using Bot.Persistence.Domain;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Bot.Persistence.EntityFrameWork.Configurations
+{
+    public class UsersConfiguration : IEntityTypeConfiguration<User>
+    {
+
+        public void Configure(EntityTypeBuilder<User> builder)
+        {
+            builder.ToTable("Users");
+            builder.HasKey(x => x.Id);
+
+            builder.Property(x => x.Id).HasColumnType("bigint").HasColumnName("Id").IsRequired();
+            builder.Property(x => x.Name).HasColumnType("nvarchar(MAX)").HasColumnName("Name").IsRequired();
+            builder.Property(x => x.SpamWarning).HasColumnType("int").HasColumnName("SpamWarning").IsRequired();
+            builder.Property(x => x.TotalTimesTimedOut).HasColumnType("int").HasColumnName("TimesTimedOut").IsRequired();
+            builder.Property(x => x.CommandUsed).HasColumnType("datetime").HasColumnName("CommandUsed").IsRequired();
+            builder.Property(x => x.CommandSpam).HasColumnType("int").HasColumnName("CommandSpam").IsRequired();
+        }
+    }
+}
