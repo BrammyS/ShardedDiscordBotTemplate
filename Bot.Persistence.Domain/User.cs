2f161f8c6c8f7ee425b4895a051f1ae6700157ba
BramEsendam <bram.esendam@gmail.com>
2019-01-22 20:56:40 +0100

    Create User.cs

---
+++ Bot.Persistence.Domain/User.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Bot.Persistence.Domain
+{
+    public class User
+    {
+
+        /// <summary>The id of the user.</summary>
+        public long Id { get; set; }
+
+        /// <summary>The username of the user.</summary>
+        public string Name { get; set; }
+
+        /// <summary>The <see cref="DateTime"/> of when the user used a command for the last time.</summary>
+        public DateTime CommandUsed { get; set; }
+
+        /// <summary>The amount of times the user was timed out for using commands to fast.</summary>
+        public int TotalTimesTimedOut { get; set; }
+
+        /// <summary>The amount of warning the user received when the use is using commands to fast.</summary>
+        public int SpamWarning { get; set; }
+
+        /// <summary>The amount of times the user was using commands to fast.</summary>
+        public int CommandSpam { get; set; }
+    }
+}
