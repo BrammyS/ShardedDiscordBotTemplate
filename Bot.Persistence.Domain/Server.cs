c54d668b74c06e5872f7a4f1537b457c733e432d
BramEsendam <bram.esendam@gmail.com>
2019-01-22 20:51:47 +0100

    Create Server.cs

---
+++ Bot.Persistence.Domain/Server.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Bot.Persistence.Domain
+{
+    public class Server
+    {
+
+        /// <summary>The id of the server.</summary>
+        public long Id { get; set; }
+
+        /// <summary>The name of the server.</summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// The custom prefix of the server.
+        /// This is an empty string or null when the server isn't using a custom prefix.
+        /// </summary>
+        public string Prefix { get; set; }
+
+        /// <summary>The amount of members in the server.</summary>
+        public int TotalMembers { get; set; }
+
+        /// <summary>The <see cref="DateTime"/> of when the bot joined this server.</summary>
+        public DateTime JoinDate { get; set; }
+
+        /// <summary>
+        /// The <see cref="bool"/> of the server to see if the bot is still the server.
+        /// This value is True if the bot is this in the server.
+        /// </summary>
+        public bool Active { get; set; }
+    }
+}
