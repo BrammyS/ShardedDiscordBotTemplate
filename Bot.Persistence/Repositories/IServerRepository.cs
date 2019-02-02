23b43548e37e359c11902cc9b2d032a9abffe04a
BramEsendam <bram.esendam@gmail.com>
2019-02-02 19:52:14 +0100

    Update IServerRepository.cs

--- Bot.Persistence/Repositories/IServerRepository.cs
+++ Bot.Persistence/Repositories/IServerRepository.cs
@@ -14,18 +14,18 @@ namespace Bot.Persistence.Repositories
         /// With all the includes.
         /// </summary>
         /// <param name="id">The id of the server.</param>
-        /// <returns>An awaitable <see cref="Task"/> that returns a <see cref="Server"/></returns>
+        /// <returns>An awaitable <see cref="Task"/> that returns a <see cref="Server"/>.</returns>
         Task<Server> GetServerAsync(ulong id);
 
 
         /// <summary>
-        /// Gets the server if it exists int he database.
-        /// And it adds the server to the database if the server doesn't exist.
+        /// Gets the server if it exists in the database.
+        /// And it adds the server to the database if it doesn't exist.
         /// </summary>
         /// <param name="id">The id of the server.</param>
         /// <param name="serverName">The name of the server.</param>
         /// <param name="memberCount">The memberCount of the server.</param>
-        /// <returns>An awaitable <see cref="Task"/> that returns a <see cref="Server"/></returns>
+        /// <returns>An awaitable <see cref="Task"/> that returns a <see cref="Server"/>.</returns>
         Task<Server> GetOrAddServer(ulong id, string serverName, int memberCount);
 
 
@@ -33,7 +33,7 @@ namespace Bot.Persistence.Repositories
         /// Gets all the custom prefixes for all the server.
         /// This should be used when the bot starts up.
         /// </summary>
-        /// <returns>An awaitable <see cref="Task"/> that returns a <see cref="List{ServerPrefix}"/></returns>
+        /// <returns>An awaitable <see cref="Task"/> that returns a <see cref="List{ServerPrefix}"/>.</returns>
         Task<List<ServerPrefix>> GetAllPrefixesAsync();
     }
 }
