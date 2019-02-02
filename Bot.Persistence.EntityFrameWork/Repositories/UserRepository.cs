ff98fc8cb9fbd80dbe9f67b8f5a033609ead7ca2
BramEsendam <bram.esendam@gmail.com>
2019-02-02 19:52:23 +0100

    Added  GetOrAddUserAsync

--- Bot.Persistence.EntityFrameWork/Repositories/UserRepository.cs
+++ Bot.Persistence.EntityFrameWork/Repositories/UserRepository.cs
@@ -1,5 +1,8 @@
-﻿using Bot.Persistence.Domain;
+﻿using System;
+using System.Threading.Tasks;
+using Bot.Persistence.Domain;
 using Bot.Persistence.Repositories;
+using Microsoft.EntityFrameworkCore;
 
 namespace Bot.Persistence.EntityFrameWork.Repositories
 {
@@ -7,6 +10,37 @@ namespace Bot.Persistence.EntityFrameWork.Repositories
     {
         public UserRepository(BotContext context) : base(context)
         {
+        }
+
+
+        /// <inheritdoc/>
+        public async Task<User> GetOrAddUserAsync(ulong id, string userName)
+        {
+            try
+            {
+                var exists = await Context.Set<User>().AnyAsync(x => x.Id == id).ConfigureAwait(false);
+
+                // Return the user if it exists in the database.
+                // Create a new one if it doesn't exist.
+                if (exists) return await Context.Set<User>().FirstOrDefaultAsync(x=>x.Id == id).ConfigureAwait(false);
+                var user = await Context.Set<User>().AddAsync(new User
+                {
+                    Id = id,
+                    Name = userName,
+                    CommandSpam = 0,
+                    CommandUsed = DateTime.Now.AddMinutes(-1),
+                    SpamWarning = 0,
+                    TotalTimesTimedOut = 0
+                    
+                }).ConfigureAwait(false);
+                await Context.SaveChangesAsync().ConfigureAwait(false);
+                return user.Entity;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
         }
     }
 }
--- Bot.Persistence/Repositories/IUserRepository.cs
+++ Bot.Persistence/Repositories/IUserRepository.cs
@@ -1,9 +1,19 @@
-﻿using Bot.Persistence.Domain;
+﻿using System.Threading.Tasks;
+using Bot.Persistence.Domain;
 
 namespace Bot.Persistence.Repositories
 {
     public interface IUserRepository : IRepository<User>
     {
+
+        /// <summary>
+        /// Gets the user if it exists in the database.
+        /// And it adds the user to the database if it doesn't exist.
+        /// </summary>
+        /// <param name="id">The id of the user.</param>
+        /// <param name="userName">The name of the user.</param>
+        /// <returns>An awaitable <see cref="Task"/> that returns a <see cref="User"/>.</returns>
+        Task<User> GetOrAddUser(ulong id, string userName);
     }
 }
 
