0248185be1339c8aab3f1941e2cabf980ae45528
BramEsendam <bram.esendam@gmail.com>
2019-02-03 13:00:14 +0100

    Implemented UpdateStatusAsync

--- Bot.BotLists/Services/BotListUpdater.cs
+++ Bot.BotLists/Services/BotListUpdater.cs
@@ -1,15 +1,57 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Collections.Concurrent;
+using System.Net.Cache;
+using System.Threading.Tasks;
+using Bot.BotLists.Configurations;
 using Bot.BotLists.Interfaces.Services;
+using Bot.Logger.Interfaces;
+using RestSharp;
+using RestSharp.Authenticators;
 
 namespace Bot.BotLists.Services
 {
     public class BotListUpdater : IBotListUpdater
     {
 
-        /// <inheritdoc />
-        public Task UpdateStatusAsync(int shardCount, int guildCount, int shardId = 0)
+        private readonly ILogger _logger;
+
+
+        private static readonly ConcurrentDictionary<string, RestClient> RestClients = new ConcurrentDictionary<string, RestClient>();
+
+
+        public BotListUpdater(ILogger logger)
         {
-            throw new System.NotImplementedException();
+            _logger = logger;
+            RestClients.TryAdd("DiscordBotsDotOrg", new RestClient
+            {
+                Authenticator = new JwtAuthenticator(RestClientsConfig.TokenConfig.DiscordBotsDotOrgToken),
+                CachePolicy = new RequestCachePolicy(RequestCacheLevel.NoCacheNoStore),
+                BaseUrl = new Uri("https://discordbots.org/api/")
+            });
+        }
+
+
+        /// <inheritdoc />
+        public async Task UpdateStatusAsync(ulong botId, int shardCount, int guildCount, int shardId = 0)
+        {
+            await UpdateDiscordBotsDotOrg(botId, shardCount, guildCount, shardId).ConfigureAwait(false);
+        }
+
+
+        private async Task UpdateDiscordBotsDotOrg(ulong botId, int shardCount, int guildCount, int shardId)
+        {
+
+            var request = new RestRequest($"bots/{botId}/stats")
+            {
+                RequestFormat = DataFormat.Json,
+                Method = Method.POST
+            };
+            request.AddOrUpdateParameter("shard_count", shardCount);
+            request.AddOrUpdateParameter("server_count", guildCount);
+            request.AddOrUpdateParameter("shard_id", shardId);
+            var result = await RestClients["DiscordBotsDotOrg"].ExecutePostTaskAsync(request).ConfigureAwait(false);
+            if (result.IsSuccessful) return;
+            _logger.Log($"Failed to update dblOrg stats reason: {result.ErrorMessage}", ConsoleColor.Red);
         }
     }
 }
