7fedfbfbc8a7218d20a687d79f6fe3cccb9c142f
BramEsendam <bram.esendam@gmail.com>
2019-02-03 13:00:01 +0100

    Update IBotListUpdater.cs

--- Bot.BotLists.Interfaces/Services/IBotListUpdater.cs
+++ Bot.BotLists.Interfaces/Services/IBotListUpdater.cs
@@ -9,11 +9,12 @@ namespace Bot.BotLists.Interfaces.Services
         /// <summary>
         /// Updates the stats for all the bot lists.
         /// </summary>
+        /// <param name="botId">The Id of the client.</param>
         /// <param name="shardCount">The total amount of shards the client has.</param>
         /// <param name="guildCount">The amount of server the client is in.</param>
         /// <param name="shardId">The shard id.</param>
         /// <returns>An awaitable <see cref="Task"/>.</returns>
-        Task UpdateStatusAsync(int shardCount, int guildCount, int shardId = 0);
+        Task UpdateStatusAsync(ulong botId, int shardCount, int guildCount, int shardId = 0);
 
     }
 }
